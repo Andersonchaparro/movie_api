b6ee57db004ec8007d01020a034c8533be84502a
Anderson <chanderson.ja@gmail.com>
2023-10-20 12:50:03 -0300

    the importing for validating token has changed

--- middlewares/jwt_bearer.py
+++ middlewares/jwt_bearer.py
@@ -1,7 +1,7 @@
 from fastapi.security import HTTPBearer
 from fastapi import Request
 from fastapi import HTTPException
-from jwt_manager import validate_token
+from utils.jwt_manager import validate_token
 
 class JWTBearer(HTTPBearer):
     async def __call__(self, request: Request):
